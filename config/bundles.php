a7a8df2c73be857d8dd05d01c720f69c3ee8e922
Alexandros-Pallis <alexanpall@gmail.com>
2026-06-13 23:01:42 +0300

    feat: image upload/serve functionality

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,6 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    League\FlysystemBundle\FlysystemBundle::class => ['all' => true],
+    Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
 ];
--- src/Entity/Tag.php
+++ src/Entity/Tag.php
@@ -125,4 +125,9 @@ class Tag
 
         return $this;
     }
+
+    public function __toString(): string
+    {
+        return $this->name;
+    }
 }
